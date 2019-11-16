d3d7ad8745d6882bd27158e79fdfa6bb8e86b0e9
Astarhel <43136296+Astarhel@users.noreply.github.com>
2019-11-16 12:56:56 +0100

    Añadidas funcionalidades de camara y demasmejoras

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Mas de un BuilgManager en la escena");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standarTurretPrefab;
+
+    void Start()
+    {
+
+        turretToBuild = standarTurretPrefab;
+        
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
