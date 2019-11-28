fe8265cb017d3f5602004c2f5fd0569df57795c4
javihollow <javierbravo1998@outlook.com>
2019-11-28 12:17:50 +0100

    interfaz con transiciones

---
+++ Assets/Scripts/Menu Scripts/UIScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject canvasContainer;
+    private Canvas[] allCanvas;
+
+    void Awake()
+    {
+        allCanvas = new Canvas[canvasContainer.transform.childCount];
+        for(int i=0; i < allCanvas.Length; i++)
+        {
+            allCanvas[i] = canvasContainer.transform.GetChild(i).GetComponent<Canvas>();
+        }
+    }
+
+    public void ChangeSceneTo(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ChangeScreenTo(int screen)
+    {
+        foreach (Canvas canvas in allCanvas)
+        {
+            canvas.enabled = false;
+        }
+
+        allCanvas[screen].enabled = true;
+    }
+}
