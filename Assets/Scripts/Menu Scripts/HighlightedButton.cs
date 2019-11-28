0deb8c57c25ab5bd3a059072641c1869b3cc67c4
javihollow <javierbravo1998@outlook.com>
2019-11-28 20:57:22 +0100

    interfaz V2

---
+++ Assets/Scripts/Menu Scripts/HighlightedButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HighlightedButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    private Text text;
+    private bool inside = false;
+    private float TIME = 0.5f;
+    private float timer = 0.5f;
+
+    void Awake()
+    {
+        text = transform.GetChild(0).GetComponent<Text>();
+    }
+
+  public void OnPointerEnter(PointerEventData eventData)
+  {
+        text = transform.GetChild(0).GetComponent<Text>();
+        text.text = ">> " + text.text;
+        inside = true;
+  }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text = transform.GetChild(0).GetComponent<Text>();
+        text.text = text.text.Substring(3, text.text.Length - 3);
+        inside = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        text = transform.GetChild(0).GetComponent<Text>();
+        if(text.text[0] != '>')
+            text.text = ">> " + text.text;
+        
+        inside = true;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (inside && timer <= 0)
+        {
+            timer = TIME;
+            text.enabled = !text.enabled;
+        }
+        else if (!inside)
+        {
+            text.enabled = true;
+            timer = TIME;
+        }
+    }
+}
--- Assets/Scripts/Menu Scripts/UIScript.cs
+++ Assets/Scripts/Menu Scripts/UIScript.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
+
 
 public class UIScript : MonoBehaviour
 {
     public GameObject canvasContainer;
+    public GameObject music;
+    public GameObject effects;
     private Canvas[] allCanvas;
 
     void Awake()
@@ -30,5 +35,28 @@ public class UIScript : MonoBehaviour
         }
 
         allCanvas[screen].enabled = true;
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        AudioSource audio = music.GetComponent<AudioSource>();
+        audio.volume = volume;
+    }
+
+    public void ChangeEffectsVolume(float volume)
+    {
+        AudioSource audio = effects.GetComponent<AudioSource>();
+        audio.volume = volume;
+    }
+
+    public void play(AudioClip clip)
+    {
+        AudioSource audio = effects.GetComponent<AudioSource>();
+        audio.PlayOneShot(clip);
     }
 }
